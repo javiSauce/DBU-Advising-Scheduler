0d564be4e05a19001b0cfce5ce2016dd295f5e70
Daniel Mendez <dmtx97@gmail.com>
2019-12-11 22:51:23 -0600

    added database

---
+++ DBU-Advising-Scheduler/Helpers/DegreeJsonHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using Newtonsoft;
+using Newtonsoft.Json;
+using DBU_Advising_Scheduler.Models;
+
+namespace DBU_Advising_Scheduler.Helpers
+{
+    public class DegreeJsonHelper
+    {
+
+        public string GetFields(Type modelType)
+        {
+            return string.Join(",",
+                modelType.GetProperties()
+                         .Select(p => p.GetCustomAttribute<JsonPropertyAttribute>())
+                         .Select(jp => jp.PropertyName));
+        }
+    }
+}
