8cb267071d77267acacfabcab17e00029760a1c2
Daniel Mendez <dmtx97@gmail.com>
2019-12-12 03:50:19 -0600

    added completedcourses database and degree plan selection

--- DBU-Advising-Scheduler/Controllers/DegreePlanController.cs
+++ DBU-Advising-Scheduler/Controllers/DegreePlanController.cs
@@ -11,11 +11,20 @@ using DBU_Advising_Scheduler.Models;
 using DBU_Advising_Scheduler.Helpers;
 using System.Web.Hosting;
 using System.Threading.Tasks;
+using DBU_Advising_Scheduler.TokenStorage;
+using System.Security.Claims;
+using Microsoft.Owin.Security.Cookies;
 
 namespace DBU_Advising_Scheduler.Controllers
 {
+
+
     public class DegreePlanController : BaseController
     {
+        private DBU_Advising_Scheduler.Models.dbuasEntities2 db = new DBU_Advising_Scheduler.Models.dbuasEntities2();
+
+
+
         // GET: DegreePlan
         public ActionResult Index()
         {
@@ -35,12 +44,12 @@ namespace DBU_Advising_Scheduler.Controllers
         //    return Content(result);
         //}
 
-        public async Task<ActionResult> Degree()
+        public async Task<ActionResult> Degree(string args)
         {
             var plan = new DegreeScraper();
-            var result = await plan.GetDegreeTable("BS Philosophy");
-
-            return Content(result);
+            var result = await plan.GetDegreeTable(args);
+            ViewBag.Plan = result;
+            return View();
         }
 
         public ActionResult SideMenu()
@@ -48,6 +57,34 @@ namespace DBU_Advising_Scheduler.Controllers
             string fileName = HostingEnvironment.MapPath(@"~\degrees.json");
             Degrees degrees = JsonConvert.DeserializeObject<Degrees>(System.IO.File.ReadAllText(fileName));
             return PartialView(degrees);
+        }
+
+        public ActionResult CoursesCompleted(string entry)
+        {
+            if (Request.IsAuthenticated)
+            {
+                // Get the user's token cache
+                var tokenStore = new SessionTokenStore(null,
+                    System.Web.HttpContext.Current, ClaimsPrincipal.Current);
+
+                if (tokenStore.HasData())
+                {
+                    // Add the user to the view bag
+                    var info = tokenStore.GetUserDetails();
+
+                    var insert = db.Set<completedcours>();
+                    insert.Add(new completedcours { DisplayName = info.DisplayName, Email = info.Email, Courses = entry });
+                    db.SaveChanges();
+                }
+            }
+
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return RedirectToAction("Index", "Calendar");
+
         }
     }
 }
